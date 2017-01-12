427ac56e7b27223c9fdadec08fc7fc988936bcfd
tuan nguyen <nguyenvantuan@nguyens-mac.local>
2017-01-12 22:57:47 +0700

    Initial Commit

---
+++ BirdFlyInJung/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BirdFlyInJung
+//
+//  Created by Nguyen Van Tuan on 1/12/17.
+//  Copyright © 2017 MyProjects. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
